4c8ab97e4f1118dc796bed7e5631b7cc117c427f
jgreen83 <42304272+jgreen83@users.noreply.github.com>
2024-11-06 23:38:04 -0500

    added garage door logic

--- Arduino/449_combine_pir_sound/449_combine_pir_sound.ino
+++ Arduino/449_combine_pir_sound/449_combine_pir_sound.ino
@@ -26,7 +26,7 @@ void setup() {
   pinMode(A0, INPUT);                            // analog pin used to connect the sensor
   tmrpcm.speakerPin = 9; //5,6,11 or 46 on Mega, 9 on Uno, Nano, etc
 
-  pinMode(A1, INPUT);
+  pinMode(A1, INPUT); //garage door sensor pin
 
   tmrpcm.setVolume(4);
   peakDetection.begin(48, 2, 0.6);               // sets the lag, threshold and influence
@@ -41,6 +41,7 @@ void setup() {
 
 void loop() {
     count = count + 1;
+    
     if(readyToDetect) {
       Serial.println("ready");
 
@@ -49,46 +50,57 @@ void loop() {
       //scaledGarage is in mV - should be ~100 mV if intact, < 1 mV (~.1mV) if broken
 
       //logic: always check garage. if garage broken (garagePOS) then enter a loop to start checking PIR for x seconds
+      if(mVolts < 80) {
+        garagePOS = true;
+      }
+      else {
+        garagePOS = false;
+      }
+
       //if PIR doesn't return anything, then set garagePOS back to false. loop restarts (readyToDetect = true)
       //if PIR does detect anything, (if(listSum...)...), trigger all deterrence
 
-
-      //this is PIR stuff
-      int arrInd = count % LEN_WINDOW;
-      
-      double data = (double)analogRead(A0)/512-1;  // converts the sensor value to a range between -1 and 1
-      peakDetection.add(data);                     // adds a new data point
-      int peak = peakDetection.getPeak();          // 0, 1 or -1
-      if(peak == 1) {
-        pirPeaks[arrInd] = abs(peak);
-      }
-      else {
-        pirPeaks[arrInd] = 0;
-      }
-      
-      double filtered = peakDetection.getFilt();   // moving average
-      Serial.print(data);                          // print data
-      Serial.print(",");
-      Serial.print(peak);                          // print peak status
-      Serial.print(",");
-      Serial.println(filtered);                    // print moving average
-      Serial.println(listSum(pirPeaks,LEN_WINDOW));
-
-      
-      
-      if(listSum(pirPeaks,LEN_WINDOW) >= LEN_WINDOW * 0.3) {
-        //ALL DETERRENCE TRIGGERS HERE
-        readyToDetect = false;
-        tmrpcm.setVolume(4);
-        tmrpcm.play("dog1.WAV");
-        Serial.println("music");
-        delay(5000);
-        tmrpcm.pause();
-        for (int i = 0; i < LEN_WINDOW; i++) {
-          pirPeaks[i] = 0;
+      if(garagePOS) {
+        startPIR = millis();
+        
+        while(millis() - startPIR < 5000) {
+          count = count + 1
+          
+          //this is PIR stuff
+          int arrInd = count % LEN_WINDOW;
+          
+          double data = (double)analogRead(A0)/512-1;  // converts the sensor value to a range between -1 and 1
+          peakDetection.add(data);                     // adds a new data point
+          int peak = peakDetection.getPeak();          // 0, 1 or -1
+          if(peak == 1) {
+            pirPeaks[arrInd] = abs(peak);
+          }
+          else {
+            pirPeaks[arrInd] = 0;
+          }
+          
+          double filtered = peakDetection.getFilt();   // moving average
+    
+          if(listSum(pirPeaks,LEN_WINDOW) >= LEN_WINDOW * 0.3) {
+            //ALL DETERRENCE TRIGGERS HERE
+            readyToDetect = false;
+            tmrpcm.setVolume(4);
+            tmrpcm.play("dog1.WAV");
+            Serial.println("music");
+            delay(5000);
+            tmrpcm.pause();
+            for (int i = 0; i < LEN_WINDOW; i++) {
+              pirPeaks[i] = 0;
+            }
+            readyToDetect = true;
+          }
         }
-        readyToDetect = true;
+
+        garagePOS = false;
+
+        
       }
+  
     }
     
 }
